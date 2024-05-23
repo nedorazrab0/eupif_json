8499ebf8049b4d8f89b84b7134d0a3199eda8a89
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-05-23 12:07:07 +0300

    Update generate_json.sh

--- generate_json.sh
+++ generate_json.sh
@@ -5,7 +5,7 @@ aapt dump xmltree euapp.apk res/xml/inject_fields.xml > ./fields.xml
 # generate json
 echo '{' > ./pif.json
 for val in PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT SECURITY_PATCH FIRST_API_LEVEL; do
-    key="$(grep -A2 "$val" ./hz.xml | sed -n 3p | awk -F 'value=' '{print $2}' | awk '{print $1}')"
+    key="$(grep -A2 "$val" ./fields.xml | sed -n 3p | awk -F 'value=' '{print $2}' | awk '{print $1}')"
     echo "    \"$val\": ${key:-\"null\"}," >> ./pif.json
 done
 sed -i '$s/.$//' ./pif.json
