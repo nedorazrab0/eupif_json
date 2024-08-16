57769500eb61d24bd7c8557d74600bf6e4b713ae
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-16 16:42:40 +0300

    Update track_link.sh

--- track_link.sh
+++ track_link.sh
@@ -3,7 +3,8 @@ while true; do
     link="$(curl --parallel-immediate --parallel-max 0 -fsZ 'https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app' | grep -om1 'https.*download')"
     if [[ -n "$link" && "$link" != "$saved_link" ]]; then
         echo "saved_link=${link}" > ./saved_link.sh
+        sleep 5
         source ./generate_json.sh
     fi
-    sleep 1
+    sleep 60
 done
