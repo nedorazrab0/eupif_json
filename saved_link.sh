e66019faf9d1eeba53800836b092362eec4f0c17
valenok <141725500+nedorazrab0@users.noreply.github.com>
2025-08-25 12:51:45 +0300

    Update saved_link.sh

--- saved_link.sh
+++ saved_link.sh
@@ -1,1 +1,1 @@
-saved_link=https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/Xiaomi.eu-app/XiaomiEUModule_2025.07.21.apk/download
+asaved_link=https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/Xiaomi.eu-app/XiaomiEUModule_2025.07.21.apk/download
